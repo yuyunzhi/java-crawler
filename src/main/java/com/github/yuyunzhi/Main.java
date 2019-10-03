2c9e68d046d78766f6203aa7e1f9ea25ac237c97
lambert1129 <lambert1129@foxmail.com>
2019-10-03 22:05:57 +0800

    添加spotbug插件

--- src/main/java/com/github/yuyunzhi/Main.java
+++ src/main/java/com/github/yuyunzhi/Main.java
@@ -18,6 +18,7 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         // 待处理的链接池
         List<String> linkPool = new ArrayList<>();
         // 已经处理过的链接池
@@ -25,19 +26,19 @@ public class Main {
 
         linkPool.add("https://sina.cn");
 
-        while(true){
+        while (true) {
 
-            if(linkPool.isEmpty()){
+            if (linkPool.isEmpty()) {
                 break;
             }
 
-            String link = linkPool.remove(linkPool.size()-1);
+            String link = linkPool.remove(linkPool.size() - 1);
 
-            if(processedLinks.contains(link)){
+            if (processedLinks.contains(link)) {
                 continue;
             }
 
-            if(isInterestingLink(link)){
+            if (isInterestingLink(link)) {
                 System.out.println("link = " + link);
                 Document doc = getHttpAndParseHtml(link);
 
@@ -45,7 +46,7 @@ public class Main {
 //                for (Element aTag: links) {
 //                    linkPool.add(aTag.attr("href"));
 //                }
-                doc.select("a").stream().map(aTag->aTag.attr("href")).forEach(linkPool::add);
+                doc.select("a").stream().map(aTag -> aTag.attr("href")).forEach(linkPool::add);
 
 
                 // 如果这是一个新闻页面，就提取页面的数据存入数据库中
@@ -53,20 +54,19 @@ public class Main {
 
                 // 处理完连接后，把处理后的链接放入processedLinks
                 processedLinks.add(link);
-            }else{
+            } else {
 
             }
         }
-
 
 
     }
 
     private static void saveDataBaseIfItisNewsPage(Document doc) {
         ArrayList<Element> articleTags = doc.select("article");
-        if(!articleTags.isEmpty()){
-            for (Element articleTag:articleTags) {
-                String title  = articleTags.get(0).child(0).text();
+        if (!articleTags.isEmpty()) {
+            for (Element articleTag : articleTags) {
+                String title = articleTags.get(0).child(0).text();
                 System.out.println("title = " + title);
             }
 
@@ -74,35 +74,37 @@ public class Main {
     }
 
     private static Document getHttpAndParseHtml(String link) throws IOException {
-        if(link.startsWith("//")){ link = "https:"+link; }
+        if (link.startsWith("//")) {
+            link = "https:" + link;
+        }
 
         CloseableHttpClient httpclient = HttpClients.createDefault();
 
         HttpGet httpGet = new HttpGet(link);
-        httpGet.addHeader("User-Agent","Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1");
+        httpGet.addHeader("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1");
 
         try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
             System.out.println(response1.getStatusLine());
             HttpEntity entity1 = response1.getEntity();
             String html = EntityUtils.toString(entity1);
 
-             return Jsoup.parse(html);
+            return Jsoup.parse(html);
         }
     }
 
     private static boolean isInterestingLink(String link) {
-        return (isNewsPage(link) && isNotLoginPage(link))|| isIndexPage(link) ;
+        return (isNewsPage(link) && isNotLoginPage(link)) || isIndexPage(link);
     }
 
-    private static boolean isNewsPage(String link){
+    private static boolean isNewsPage(String link) {
         return link.contains("news.sina.cn");
     }
 
-    private static boolean isNotLoginPage(String link){
+    private static boolean isNotLoginPage(String link) {
         return !link.contains("passport.sina.cn");
     }
 
-    private static boolean isIndexPage(String link){
+    private static boolean isIndexPage(String link) {
         return "https://sina.cn".equals(link);
     }
 
